4b477626b274bd35b1970028a606b834e76d9dee
Minhvo96 <voquangminhbck38@gmail.com>
2024-01-10 13:46:41 +0700

    update

--- src/main/java/com/codegym/spb_eyesclinic_project/service/user/UserService.java
+++ src/main/java/com/codegym/spb_eyesclinic_project/service/user/UserService.java
@@ -1,9 +1,4 @@
 //package com.codegym.spb_eyesclinic_project.service.user;
-//
-//
-//import com.codegym.spb_eyesclinic_project.domain.Customer;
-//import com.codegym.spb_eyesclinic_project.domain.Enum.ERole;
-//import com.codegym.spb_eyesclinic_project.domain.Staff;
 //import com.codegym.spb_eyesclinic_project.domain.User;
 //import com.codegym.spb_eyesclinic_project.repository.CustomerRepository;
 //import com.codegym.spb_eyesclinic_project.repository.StaffRepository;
@@ -38,49 +33,49 @@
 //    }
 //
 //    public void createUser(UserSaveRequest request) {
-////        return userRepository.save(user);
-//
+//        return userRepository.save(user);
+
 //        var user = AppUtils.mapper.map(request, User.class);
 //        userRepository.save(user);
+
+        // lấy role trong request > kiểm tra nếu mà Staff thì call StaffRepo.save, nếu mà Cus thì call CusRepo.save
+        // staff(userId, phoneNumber) cus(userId, phoneNumber)
+
+//        if ("Staff".equals(request.getRole())) {
+//            var staff = AppUtils.mapper.map(request, Staff.class);
+//            staffRepository.save(staff);
+//        } else if ("Cus".equals(request.getRole())) {
+//            var customer = AppUtils.mapper.map(request, Customer.class);
+//            customerRepository.save(customer);
+//        }
+
+//        ERole role = ERole.valueOf(request.getRole());
+//        switch (role) {
+//            case ROLE_USER:
+//                var customer = AppUtils.mapper.map(request, Customer.class);
+//                customer.setUser(user);
+//                customerRepository.save(customer);
+//                break;
+//            case ROLE_ADMIN:
+//            case ROLE_RECEPTIONIST:
+//            case ROLE_DOCTOR:
+//                var staff = AppUtils.mapper.map(request, Staff.class);
+//                staff.setUser(user);
+//                staffRepository.save(staff);
+//                break;
+//            default:
 //
-//        // lấy role trong request > kiểm tra nếu mà Staff thì call StaffRepo.save, nếu mà Cus thì call CusRepo.save
-//        // staff(userId, phoneNumber) cus(userId, phoneNumber)
+//                break;
+//        }
+//    }
 //
-////        if ("Staff".equals(request.getRole())) {
-////            var staff = AppUtils.mapper.map(request, Staff.class);
-////            staffRepository.save(staff);
-////        } else if ("Cus".equals(request.getRole())) {
-////            var customer = AppUtils.mapper.map(request, Customer.class);
-////            customerRepository.save(customer);
-////        }
+//    public String getUserRoleByPhoneNumber(String phoneNumber) {
+//        User user = userRepository.findUserByPhoneNumber(phoneNumber)
+//                .orElseThrow(() -> new RuntimeException("Not found!"));
 //
-////        ERole role = ERole.valueOf(request.getRole());
-////        switch (role) {
-////            case ROLE_USER:
-////                var customer = AppUtils.mapper.map(request, Customer.class);
-////                customer.setUser(user);
-////                customerRepository.save(customer);
-////                break;
-////            case ROLE_ADMIN:
-////            case ROLE_RECEPTIONIST:
-////            case ROLE_DOCTOR:
-////                var staff = AppUtils.mapper.map(request, Staff.class);
-////                staff.setUser(user);
-////                staffRepository.save(staff);
-////                break;
-////            default:
-////
-////                break;
-////        }
-////    }
-////
-////    public String getUserRoleByPhoneNumber(String phoneNumber) {
-////        User user = userRepository.findUserByPhoneNumber(phoneNumber)
-////                .orElseThrow(() -> new RuntimeException("Not found!"));
-////
-////        // Lấy vai trò từ đối tượng User
-////        var role = String.valueOf(user.getRole());
-////
-////        return role;
-////    }
+//        // Lấy vai trò từ đối tượng User
+//        var role = String.valueOf(user.getRole());
+//
+//        return role;
+//    }
 //}
